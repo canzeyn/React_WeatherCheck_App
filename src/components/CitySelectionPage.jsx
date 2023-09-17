f26599ac08438dd150aac0e1009062bb3987690f
canzeyn <zeyn1616@outlook.com>
2023-09-17 20:08:01 +0300

    ülke ve şehir filtreleme yapıldı responsive arttırlıdı

--- src/components/CitySelectionPage.jsx
+++ src/components/CitySelectionPage.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
+import {AiOutlineArrowLeft} from "react-icons/ai"
+import { useNavigate } from 'react-router-dom';
 
 const CitySelectionPage = () => {
 
+  const navigate = useNavigate();
+
+  const [searchTermCities , setSearchTermCities] = useState("");
+
     const cities = useSelector(state => state.data.cities);
+
+    const filteredCities = cities.filter(city => city.state_name.toLowerCase().includes(searchTermCities.toLowerCase()))
+
+    const goToCountryPage = () => {
+           navigate(-1)
+    }
 
 
   return (
    
-<div className="flex flex-col justify-center items-center min-h-screen"  style={{ backgroundImage: 'url("/images/genisEkranArkaPlan.png")'  }}> 
+<div className="flex flex-col justify-center items-center min-h-screen"  style={{ backgroundImage: 'url("/images/genisEkranArkaPlan.png")' , backgroundRepeat:"no-repeat", backgroundSize:"cover"  }}> 
 
-  <div>
-    <h3 className="text-center font-bold text-3xl  text-cream pb-5">Şehir Seçiniz</h3>
+  <div className="flex flex-row justify-between  items-center w-full ">
+    <div>
+      <AiOutlineArrowLeft onClick={goToCountryPage} className="w-12 h-8 fill-cream stroke-2 cursor-pointer" />
+    </div>
+    <div className="w-full">
+      <h3 className="text-center font-bold text-3xl  text-cream pb-5 mt-2">Şehir Seçiniz</h3>
+    </div>
+
+    <div className="w-12"></div>
+    
   </div>
 
-  <div className="grid grid-cols-5 gap-3 m-1" >
-  {cities && cities.map((city) => (
+  <div className=" flex flex-row justify-center items-center w-full pt-2 pb-10">
+    <input value={searchTermCities} onChange={e => setSearchTermCities(e.target.value)} className="bg-cream opacity-60 focus:opacity-100 text-earthyBrown rounded-md h-12 w-1/2 lg:w-2/5  xl:w-1/3 outline-none xl:text-xl pl-3"></input>
+  </div>
+
+  <div className="grid grid-cols-1 2sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 m-1" >
+  {filteredCities && filteredCities.map((city) => (
     <div key={city.id} className="flex flex-row items-center justify-center rounded-md space-x-2 px-3 py-1 cursor-pointer" style={{backgroundColor:'rgba(255,217,90,0.5)'}}>
       <p className="text-cream text-lg ">{city.state_code}</p>
-       <p className="text-cream text-xl truncate" key={city.id}>{city.state_name}</p>
+       <p className="text-cream text-xl truncate">{city.state_name}</p>
     </div>
    
   ))}
--- src/components/CountrySelectionPage.jsx
+++ src/components/CountrySelectionPage.jsx
@@ -9,45 +9,57 @@ import { fetchCitiesData } from './redux/dataSlice';
 
 const CountrySelectionPage = () => {
 
-  const [countryISO2 , setCountryISO2] = useState("");
+  const [countryISO2, setCountryISO2] = useState("");
+  const [searchTermCountry, setSearchTermCountry] = useState("");
 
   const dispatch = useDispatch(); //burada dispatch tanımlandı
   const navigate = useNavigate();
   const countries = useSelector(state => state.data.countries); //burada redux içinden ülke isimlerinin olduğu state getiriyoruz
 
+
   useEffect(() => {
-    dispatch(fetchCountriesData()); //burada dispatch ile redux içinden ülke isimlerini çekme işlemini tetikliyoruz yani çalıştırıyoruz thunk işlemini
-  }, []); //burada dispatch bağlamıyla ilk olarak component mount olduğunda çalışıyor ve duruyor ardından state güncellenirse countries adlı state tekarar çalışıyor ama bizim verilerimiz statik olduğu için state değişmeyecek 
+    if (!countries.length) {
+      dispatch(fetchCountriesData());
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(countries);
+  }, [countries]);
+
+  const filteredCountry = countries ? countries.filter(country => country && country.name &&  country.name.toLowerCase().includes(searchTermCountry.toLowerCase())) : [];
 
 
-   const getCountryISO2 = (iso2 , event) => {
+
+  const getCountryISO2 = (iso2, event) => {
     event.preventDefault();
-         setCountryISO2(iso2); 
-         navigate(`/sehirSecimSayfası/${iso2}`)
+    setCountryISO2(iso2);
+    navigate(`/sehirSecimSayfası/${iso2}`)
 
-         dispatch(fetchCitiesData(iso2));
-         
-   }
+    dispatch(fetchCitiesData(iso2));
 
-   useEffect(() => {
-     console.log(countries)
-   },[countries])
-  
-   
+  }
+
+
+
   return (
-    <div className="flex flex-col justify-center items-center " style={{ backgroundImage: 'url("images/genisEkranArkaPlan.png")'  }}>
+    <div className="flex flex-col justify-center items-center min-h-screen " style={{ backgroundImage: 'url("images/genisEkranArkaPlan.png")', backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
       <p className="text-center font-bold text-3xl m-3 text-cream">Ülke Seçiniz</p>
 
- <div className="grid grid-cols-1  sm:grid-cols-2 2md:grid-cols-3 2lg:grid-cols-4 2xl:grid-cols-5 gap-3 m-2 ">
-      {countries && countries.map((data) => (
-       
-       <div onClick={(event) => getCountryISO2(data.iso2 , event)} key={data.id} className=" flex flex-row justify-center items-center space-x-2  rounded-md cursor-pointer " style={{backgroundColor:'rgba(255,217,90,0.5)'}}>
-         <p className="text-2xl">{data.emoji}</p>
-         <p className="truncate text-cream text-md">{data.name}</p>
-       </div>
-          
-      ))}
-</div>
+      <div className=" flex flex-row justify-center items-center w-full pt-2 pb-10">
+        <input value={searchTermCountry} onChange={e => setSearchTermCountry(e.target.value)} className="bg-cream opacity-60 focus:opacity-100 text-earthyBrown rounded-md h-12 w-1/2 lg:w-2/5  xl:w-1/3 outline-none xl:text-xl pl-3"></input>
+      </div>
+
+      <div className="grid grid-cols-1  sm:grid-cols-2 2md:grid-cols-3 2lg:grid-cols-4 2xl:grid-cols-5 gap-3 m-2 ">
+        {filteredCountry && filteredCountry.map((data) => (
+
+          <div onClick={(event) => getCountryISO2(data.iso2, event)} key={data.id} className=" flex flex-row justify-center items-center space-x-2  rounded-md cursor-pointer " style={{ backgroundColor: 'rgba(255,217,90,0.5)' }}>
+            <p className="text-2xl">{data.emoji}</p>
+            <p className="truncate text-cream text-md">{data.name}</p>
+          </div>
+
+        ))}
+      </div>
 
 
     </div>
